f06cb76e473d1e5202796e09d7101ba2c7bd56d2
Dauren Assanbaev <sharinganyt430@gmail.com>
2025-05-03 23:19:16 +0500

    added func

--- src/main/java/com/ecommerce/wehackbackend/service/OrganizerRequestService.java
+++ src/main/java/com/ecommerce/wehackbackend/service/OrganizerRequestService.java
@@ -1,11 +1,14 @@
 package com.ecommerce.wehackbackend.service;
 
+import com.ecommerce.wehackbackend.exception.ResourceNotFoundException;
 import com.ecommerce.wehackbackend.model.dto.request.OrganizerRequestDto;
 import com.ecommerce.wehackbackend.model.entity.Club;
 import com.ecommerce.wehackbackend.model.entity.OrganizerRequest;
+import com.ecommerce.wehackbackend.model.entity.Role;
 import com.ecommerce.wehackbackend.model.entity.User;
 import com.ecommerce.wehackbackend.repository.ClubRepository;
 import com.ecommerce.wehackbackend.repository.OrganizerRequestRepository;
+import com.ecommerce.wehackbackend.repository.RoleRepository;
 import com.ecommerce.wehackbackend.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -24,13 +28,14 @@ public class OrganizerRequestService {
     private final OrganizerRequestRepository organizerRequestRepository;
     private final ClubRepository clubRepository;
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
     @Transactional
     public void submitRequest(OrganizerRequestDto dto) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String email = auth.getName();
 
-        User user = userRepository.findByEmail(email)
+        User user = userRepository.findByEmailAndIsActive(email)
                 .orElseThrow(() -> new EntityNotFoundException("User not found"));
 
         Club club = clubRepository.findById(dto.clubId())
@@ -60,5 +65,17 @@ public class OrganizerRequestService {
         request.setStatus(newStatus);
         request.setReviewedAt(LocalDateTime.now());
         organizerRequestRepository.save(request);
+
+        if (newStatus.equals(OrganizerRequest.Status.APPROVED)) {
+            String email = request.getUser().getEmail();
+            User user = userRepository.findByEmailAndIsActive(email)
+                    .orElseThrow(() -> new ResourceNotFoundException("User", "email", email));
+
+            Role role = roleRepository.findByName("CLUB_MANAGER").orElseThrow(() -> new ResourceNotFoundException("Role", "role", "CLUB_MANAGER"));
+
+            user.setRoles(Set.of(role));
+
+            userRepository.save(user);
+        }
     }
 }
